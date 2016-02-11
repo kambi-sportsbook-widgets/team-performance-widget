8eba789c859d0ba75007dd440a11ead53d39397f
Daniel Hoffmann <daniel.hoffmann@globalmouth.com>
2016-02-11 14:55:45 -0200

    Initial commit

---
+++ src/js/controllers/appController.js
@@ -0,0 +1,100 @@
+(function () {
+
+   'use strict';
+
+   var vine_supported_languages = [
+      'en', 'sv', 'fr', 'de', 'ja', 'nl', 'it', 'es', 'pt', 'pt-PT', 'da', 'fi', 'nb', 'ko',
+      'zh-Hans', 'zh-Hant', 'ru', 'pl', 'tr', 'uk', 'ar', 'hr', 'cs', 'el', 'he', 'ro', 'sk', 'th',
+      'id', 'ms', 'en-GB', 'ca', 'hu', 'vi', 'en-us'
+   ];
+
+   (function ( $app ) {
+      return $app.controller('appController',
+         ['$scope', '$controller', '$http', '$sce', 'kambiAPIService', 'kambiWidgetService',
+            function ( $scope, $controller, $http, $sce, kambiAPIService, kambiWidgetService ) {
+
+               angular.extend(this, $controller('widgetCoreController', {
+                  '$scope': $scope
+               }));
+
+               // Default arguments, these will be overridden by the arguments from the widget api
+               $scope.defaultArgs = {
+                  title: 'Football - Team Performance Indicator',
+                  numberMatchesPerTeam: 6,
+               };
+
+               // The current height of the widget
+               $scope.currentHeight = 450;
+
+
+
+               // Call the init method in the coreWidgetController so that we setup everything using our overridden values
+               // The init-method returns a promise that resolves when all of the configurations are set, for instance the $scope.args variables
+               // so we can call our methods that require parameters from the widget settings after the init method is called
+               $scope.init().then(function () {
+                  var teams = window.mockdata.tournaments[0].teams;
+                  var teamsInfo = [];
+                  teams.forEach(function(team) {
+                     var matchHistory= [];
+
+                     //sorting by date descending
+                     team.matchHistory.sort(function(a, b) {
+                        //TODO check if parsing is done correctly
+                        var aDate = new Date(a.date);
+                        var bDate = new Date(b.date);
+                        if (aDate.getTime() > bDate.getTime())
+                           return 1;
+                        else
+                           return -1;
+                     });
+
+                     team.matchHistory.forEach(function(match, index) {
+                        if (matchHistory.length >= $scope.args.numberMatchesPerTeam)
+                           return;
+                        var isInHistory = _.find(matchHistory, function(e) {
+                           if (match.id === e.id)
+                              return true;
+                        });
+                        if (isInHistory == null) {
+                           var item = {
+                              id: match.id,
+                              date: match.date,
+                              inHome: match.homeTeam === team.name,
+                              homeTeam: match.homeTeam,
+                              awayTeam: match.awayTeam,
+                              result: "lose",
+                           };
+                           var result = match.result; //TODO can match.result be null? if so special handling is required
+
+                           if (item.inHome) {
+                              item.teamScore = result.homeScore;
+                              item.opponentScore = result.awayScore;
+                              item.opponentName = match.awayTeam;
+                           } else {
+                              item.teamScore = result.awayScore;
+                              item.opponentScore = result.homeScore;
+                              item.opponentName = match.homeTeam;
+                           }
+
+                           if (result.homeScore === result.awayScore)
+                              item.result = "draw";
+                           else if (item.inHome && result.homeScore > result.awayScore)
+                              item.result = "win";
+                           else if (!item.inHome && result.awayScore > result.homeScore)
+                              item.result = "win";
+
+                           matchHistory.push(item)
+                        }
+                     });
+                     teamsInfo.push({
+                        name: team.name,
+                        matchHistory: matchHistory,
+                     });
+                  });
+
+                  $scope.teams = teamsInfo;
+               });
+
+            }]);
+   })(angular.module('teamPerformanceWidget'));
+})($);
---
+++ src/js/mockindividualmatches.js
@@ -0,0 +1,62 @@
+//used to create mockdata.js (mockdata is still create manually)
+
+{
+   "id": 1,
+   "date": "2015-10-10T02:00:00+01:00",
+   "homeTeam": "Newcastle",
+   "awayTeam": "West ham",
+   "result": {
+      "homeScore": 1,
+      "awayScore": 2,
+   }
+},
+{
+   "id": 2,
+   "date": "2015-10-11T02:00:00+01:00",
+   "homeTeam": "Newcastle",
+   "awayTeam": "FC Barcelona",
+   "result": {
+      "homeScore": 2,
+      "awayScore": 2,
+   }
+},
+{
+   "id": 3,
+   "date": "2015-10-12T02:00:00+01:00",
+   "homeTeam": "Arsenal",
+   "awayTeam": "Newcastle",
+   "result": {
+      "homeScore": 3,
+      "awayScore": 2,
+   }
+},
+{
+   "id": 4,
+   "date": "2015-10-13T02:00:00+01:00",
+   "homeTeam": "West ham",
+   "awayTeam": "FC Barcelona",
+   "result": {
+      "homeScore": 0,
+      "awayScore": 1
+   }
+},
+{
+   "id": 5,
+   "date": "2015-10-14T02:00:00+01:00",
+   "homeTeam": "Arsenal",
+   "awayTeam": "West ham",
+   "result": {
+      "homeScore": 1,
+      "awayScore": 0
+   }
+},
+{
+   "id": 6,
+   "date": "2015-10-15T02:00:00+01:00",
+   "homeTeam": "FC Barcelona",
+   "awayTeam": "Arsenal",
+   "result": {
+      "homeScore": 3,
+      "awayScore": 3,
+   }
+},
