e8a424d5836fe028a39ac7f485009ce5a849fede
Daniel Hoffmann <daniel.hoffmann@globalmouth.com>
2016-02-15 16:42:09 -0200

    Refactored CSS and added pagination, pagination not fully working yet

--- src/js/controllers/appController.js
+++ src/js/controllers/appController.js
@@ -1,12 +1,7 @@
+/* globals _ */
 (function () {
 
    'use strict';
-
-   var vine_supported_languages = [
-      'en', 'sv', 'fr', 'de', 'ja', 'nl', 'it', 'es', 'pt', 'pt-PT', 'da', 'fi', 'nb', 'ko',
-      'zh-Hans', 'zh-Hant', 'ru', 'pl', 'tr', 'uk', 'ar', 'hr', 'cs', 'el', 'he', 'ro', 'sk', 'th',
-      'id', 'ms', 'en-GB', 'ca', 'hu', 'vi', 'en-us'
-   ];
 
    var parseMatchHistoryItem = function(match, teamName) {
       var item = {
@@ -17,17 +12,19 @@
          awayTeam: match.awayTeam,
          homeScore: match.result.homeScore,
          awayScore: match.result.awayScore,
-         result: "lose",
-         resultText: "L", //TODO internationalize this value
+         result: 'lose',
+         resultText: 'L', //TODO internationalize this value
       };
       var d = item.date;
       var day = d.getUTCDate() +'';
-      if (day.length === 1)
+      if (day.length === 1) {
          day = '0'+day;
+      }
 
       var month = (d.getUTCMonth()+1) +'';
-      if (month.length === 1)
+      if (month.length === 1) {
          month = '0'+month;
+      }
 
       item.dateText = day + '/' + month + '/' + d.getUTCFullYear();
 
@@ -44,47 +41,50 @@
       }
 
       if (result.homeScore === result.awayScore) {
-         item.result = "draw";
-         item.resultText = "D"; //TODO internationalize this value
+         item.result = 'draw';
+         item.resultText = 'D'; //TODO internationalize this value
       } else if (
          (item.inHome && result.homeScore > result.awayScore) ||
          (!item.inHome && result.awayScore > result.homeScore)
          ) {
-         item.result = "win";
-         item.resultText = "W";//TODO internationalize this value
+         item.result = 'win';
+         item.resultText = 'W';//TODO internationalize this value
       }
-      item.tooltipText = "vs " + item.opponentName + " - " + item.teamScore + "x" + item.opponentScore;
+      item.tooltipText = 'vs ' + item.opponentName + ' - ' + item.teamScore + 'x' + item.opponentScore;
       return item;
    };
 
    // returns true if the item is not already present in the provided matchHistory and
    // the matchHistory length is not bigger than args.numberMatchesPerTeam
    var shouldBeAddedToHistory = function(matchHistory, item, numberMatchesPerTeam) {
-      if (matchHistory.length >= numberMatchesPerTeam)
+      if (matchHistory.length >= numberMatchesPerTeam) {
          return false;
+      }
       //just making sure that the match is not already in there
       var isInHistory = _.find(matchHistory, function(e) {
-         if (item.id === e.id)
-            return true;
+         return item.id === e.id;
       });
-      if (isInHistory != null)
+      if (isInHistory != null) {
          return false;
+      }
       return true;
    };
 
    //calculates the performance of the team
    var calculatePerformance = function(matchHistory) {
-      let sum = 0;
+      var sum = 0;
       matchHistory.forEach(function(m) {
-         if (m.result === "win")
+         if (m.result === 'win') {
             sum += 20;
-         else if (m.result === "draw")
-            sum += 10
+         } else if (m.result === 'draw') {
+            sum += 10;
+         }
       });
       //TODO check if this calculation is correct
       return sum / (matchHistory.length * 20);
    };
 
+   // parses the data provided considering only the first numberMatchesPerTeam matches
    var parseTeamsInfo = function(teams, numberMatchesPerTeam) {
       var teamsInfo = [];
       teams.forEach(function(team) {
@@ -100,28 +100,33 @@
 
          //sorting by date descending
          team.matchHistory.sort(function(a, b) {
-            if (a.date.getTime() > b.date.getTime())
+            if (a.date.getTime() > b.date.getTime()) {
                return 1;
-            else
+            } else {
                return -1;
+            }
          });
 
          team.matchHistory.forEach(function(match, index) {
             var item = parseMatchHistoryItem(match, team.name);
 
-            if (shouldBeAddedToHistory(matchHistory, item, numberMatchesPerTeam))
+            if (shouldBeAddedToHistory(matchHistory, item, numberMatchesPerTeam)) {
                matchHistory.push(item);
+            }
 
-            if (item.inHome && shouldBeAddedToHistory(matchHistoryHome, item, numberMatchesPerTeam))
+            if (item.inHome && shouldBeAddedToHistory(matchHistoryHome, item, numberMatchesPerTeam)) {
                matchHistoryHome.push(item);
+            }
 
-            if ( !item.inHome && shouldBeAddedToHistory(matchHistoryAway, item, numberMatchesPerTeam))
+            if ( !item.inHome && shouldBeAddedToHistory(matchHistoryAway, item, numberMatchesPerTeam)) {
                matchHistoryAway.push(item);
+            }
          });
 
          teamsInfo.push({
             name: team.name,
-            performance: calculatePerformance(matchHistory, numberMatchesPerTeam),
+            performance: calculatePerformance(matchHistory),
+            detailed: false, //controls if detailed information is visible
             matchHistory: matchHistory,
             matchHistoryHome: matchHistoryHome,
             matchHistoryAway: matchHistoryAway,
@@ -130,37 +135,60 @@
       return teamsInfo;
    };
 
-   (function ( $app ) {
-      return $app.controller('appController',
-         ['$scope', '$controller', '$http', '$sce', 'kambiAPIService', 'kambiWidgetService',
-            function ( $scope, $controller, $http, $sce, kambiAPIService, kambiWidgetService ) {
+   var $app = angular.module('teamPerformanceWidget');
 
-               angular.extend(this, $controller('widgetCoreController', {
-                  '$scope': $scope
-               }));
+   $app.controller('appController',
+      ['$scope', '$controller', '$http', '$sce', 'kambiAPIService', 'kambiWidgetService',
+      function ( $scope, $controller, $http, $sce, kambiAPIService, kambiWidgetService ) {
 
-               // Default arguments, these will be overridden by the arguments from the widget api
-               $scope.defaultArgs = {
-                  title: 'Football - Team Performance Indicator',
-                  numberMatchesPerTeam: 6,
-               };
+         angular.extend(this, $controller('widgetCoreController', {
+            '$scope': $scope
+         }));
 
-               // The current height of the widget
-               $scope.currentHeight = 450;
+         // Default arguments, these will be overridden by the arguments from the widget api
+         $scope.defaultArgs = {
+            title: 'Football - Team Performance Indicator',
+            numberMatchesPerTeam: 6, // maximum number of matches to show per team
+            listLimit: 3, // Set the list limit value to be used for pagination
+         };
 
-               $scope.Math = window.Math; //to be able to use Math functions in the template
+         // The current height of the widget
+         $scope.currentHeight = 450;
 
+         $scope.Math = window.Math; //to be able to use Math functions in the template
 
-               // Call the init method in the coreWidgetController so that we setup everything using our overridden values
-               // The init-method returns a promise that resolves when all of the configurations are set, for instance the $scope.args variables
-               // so we can call our methods that require parameters from the widget settings after the init method is called
-               $scope.init().then(function () {
-                  var teams = window.mockdata.tournaments[0].teams;
-                  var teamsInfo = parseTeamsInfo(teams);
+         $scope.startFrom = 0;
+         $scope.teams = [];
 
-                  $scope.teams = teamsInfo;
-               });
+         $scope.adjustHeight = function() {
+            //TODO maybe try to dinamically get these values?
+            //might not be possible to get detailedViewHeight due to the accordion animation
+            var headerHeight = 40;
+            var footerHeight = 40;
+            var mainPadding = 16;
+            var compactViewHeight = 201;
+            var detailedViewHeight = compactViewHeight + 460;
+            var contentHeight = headerHeight + footerHeight + mainPadding + 1;
+            var teams = $scope.teams;
+            var i = $scope.startFrom;
+            while (i < $scope.startFrom + $scope.args.listLimit && i < teams.length) {
+               contentHeight += teams[i].detailed ? detailedViewHeight : compactViewHeight;
+               i++;
+            }
+            $scope.setWidgetHeight(contentHeight);
+         };
 
-            }]);
-   })(angular.module('teamPerformanceWidget'));
+         $scope.init().then(function () {
+            $http({
+               method: 'GET',
+               url: './mockdata.json'
+            }).then(function(response) {
+               var teams = response.data.tournaments[0].teams;
+               var teamsInfo = parseTeamsInfo(teams);
+               $scope.teams = teamsInfo;
+               $scope.setPages($scope.teams, $scope.args.listLimit);
+               $scope.adjustHeight();
+            });
+         });
+      }]);
 })($);
