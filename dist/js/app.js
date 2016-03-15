21630bb35e36d4a85aec97251536d15c28959fb1
Daniel Hoffmann <daniel.hoffmann@globalmouth.com>
2016-03-15 15:45:23 -0300

    Initial rivets/stapes implementation

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,210 +1,208 @@
 (function () {
+'use strict';
 
-   var arrDependencies;
+/**
+ * CoreLibrary code
+ */
+var Model = Stapes.subclass();
 
-   arrDependencies = [
-      'widgetCore',
-      'widgetCore.translate'
-   ];
+var CoreLibrary = {
+   init: function () {
+      var initModel = new Model();
 
-   (function ($app) {
-      'use strict';
+      setTimeout(function () { // "ajax" call
+         var apiData = {
+            filter: 'football/england/premier_league/',
+            args: {}
+         };
+         initModel.emit('ready', apiData);
+      }, 100);
 
-      return $app;
-   })(angular.module('teamPerformanceWidget', arrDependencies));
-}).call(this);
+      return initModel;
+   },
 
-/* globals _ */
-(function () {
+   getData: function (type, filter) {
+      var statisticsModel = new Model();
 
-   'use strict';
-
-   var parseMatchHistoryItem = function (match, teamName) {
-      var item = {
-         id: match.id,
-         date: match.date,
-         inHome: match.homeTeam === teamName,
-         homeTeam: match.homeTeam,
-         awayTeam: match.awayTeam,
-         homeScore: match.result.homeScore,
-         awayScore: match.result.awayScore,
-         result: 'lose'
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', 'mockdata.json');
+      xhr.send(null);
+      xhr.onreadystatechange = function () {
+         if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+               statisticsModel.emit('change', JSON.parse(xhr.responseText));
+            } else {
+               void 0;
+            }
+         }
       };
-      var d = item.date;
-      var day = d.getUTCDate() + '';
-      if (day.length === 1) {
-         day = '0' + day;
-      }
+      return statisticsModel;
+   }
+};
 
-      var month = (d.getUTCMonth() + 1) + '';
-      if (month.length === 1) {
-         month = '0' + month;
-      }
+rivets.formatters.translate = function (value) {
+   return value;
+};
 
-      item.dateText = day + '/' + month + '/' + d.getUTCFullYear();
+/**
+ * Widget code
+ */
 
-      var result = match.result; // TODO can match.result be null? if so special handling is required
-
-      if (item.inHome) {
-         item.teamScore = result.homeScore;
-         item.opponentScore = result.awayScore;
-         item.opponentName = match.awayTeam;
-      } else {
-         item.teamScore = result.awayScore;
-         item.opponentScore = result.homeScore;
-         item.opponentName = match.homeTeam;
-      }
-
-      if (result.homeScore === result.awayScore) {
-         item.result = 'draw';
-      } else if (
-         (item.inHome && result.homeScore > result.awayScore) ||
-         (!item.inHome && result.awayScore > result.homeScore)
-         ) {
-         item.result = 'win';
-      }
-      item.tooltipText = 'vs ' + item.opponentName + ' - ' + item.teamScore + 'x' + item.opponentScore;
-      return item;
+var parseMatchHistoryItem = function (match, teamName) {
+   var item = {
+      id: match.id,
+      date: match.date,
+      inHome: match.homeTeam === teamName,
+      homeTeam: match.homeTeam,
+      awayTeam: match.awayTeam,
+      homeScore: match.result.homeScore,
+      awayScore: match.result.awayScore,
+      result: 'lose'
    };
+   var d = item.date;
+   var day = d.getUTCDate() + '';
+   if (day.length === 1) {
+      day = '0' + day;
+   }
 
-   // Returns true if the item is not already present in the provided matchHistory and
-   // the matchHistory length is not bigger than args.numberMatchesPerTeam
-   var shouldBeAddedToHistory = function (matchHistory, item, numberMatchesPerTeam) {
-      if (matchHistory.length >= numberMatchesPerTeam) {
-         return false;
+   var month = (d.getUTCMonth() + 1) + '';
+   if (month.length === 1) {
+      month = '0' + month;
+   }
+
+   item.dateText = day + '/' + month + '/' + d.getUTCFullYear();
+
+   var result = match.result; // TODO can match.result be null? if so special handling is required
+
+   if (item.inHome) {
+      item.teamScore = result.homeScore;
+      item.opponentScore = result.awayScore;
+      item.opponentName = match.awayTeam;
+   } else {
+      item.teamScore = result.awayScore;
+      item.opponentScore = result.homeScore;
+      item.opponentName = match.homeTeam;
+   }
+
+   if (result.homeScore === result.awayScore) {
+      item.result = 'draw';
+   } else if (
+      (item.inHome && result.homeScore > result.awayScore) ||
+      (!item.inHome && result.awayScore > result.homeScore)
+   ) {
+      item.result = 'win';
+   }
+   item.tooltipText = 'vs ' + item.opponentName + ' - ' + item.teamScore + 'x' + item.opponentScore;
+   return item;
+};
+
+// Returns true if the item is not already present in the provided matchHistory and
+// the matchHistory length is not bigger than args.numberMatchesPerTeam
+var shouldBeAddedToHistory = function (matchHistory, item, numberMatchesPerTeam) {
+   if (matchHistory.length >= numberMatchesPerTeam) {
+      return false;
+   }
+   // Just making sure that the match is not already in there
+   var isInHistory = matchHistory.filter(function (e) {
+      return item.id === e.id;
+   }).length > 0;
+
+   if (isInHistory) {
+      return false;
+   }
+   return true;
+};
+
+// Calculates the performance of the team
+var calculatePerformance = function (matchHistory) {
+   var sum = 0;
+   matchHistory.forEach(function (m) {
+      if (m.result === 'win') {
+         sum += 20;
+      } else if (m.result === 'draw') {
+         sum += 10;
       }
-      // Just making sure that the match is not already in there
-      var isInHistory = matchHistory.filter(function (e) {
-         return item.id === e.id;
-      }).length > 0;
+   });
+   // TODO check if this calculation is correct
+   return sum / (matchHistory.length * 20);
+};
 
-      if (isInHistory) {
-         return false;
-      }
-      return true;
-   };
+// Parses the data provided considering only the first numberMatchesPerTeam matches
+var parseTeamsInfo = function (teams, numberMatchesPerTeam) {
+   var teamsInfo = [];
+   teams.forEach(function (team) {
+      var matchHistory = [];
+      var matchHistoryHome = [];
+      var matchHistoryAway = [];
 
-   // Calculates the performance of the team
-   var calculatePerformance = function (matchHistory) {
-      var sum = 0;
-      matchHistory.forEach(function (m) {
-         if (m.result === 'win') {
-            sum += 20;
-         } else if (m.result === 'draw') {
-            sum += 10;
+      // Parsing date strings
+      team.matchHistory.forEach(function (match, index) {
+         // TODO check if parsing is done correctly
+         match.date = new Date(match.date);
+      });
+
+      // Sorting by date descending
+      team.matchHistory.sort(function (a, b) {
+         if (a.date.getTime() > b.date.getTime()) {
+            return 1;
+         } else {
+            return -1;
          }
       });
-      // TODO check if this calculation is correct
-      return sum / (matchHistory.length * 20);
-   };
 
-   // Parses the data provided considering only the first numberMatchesPerTeam matches
-   var parseTeamsInfo = function (teams, numberMatchesPerTeam) {
-      var teamsInfo = [];
-      teams.forEach(function (team) {
-         var matchHistory = [];
-         var matchHistoryHome = [];
-         var matchHistoryAway = [];
+      team.matchHistory.forEach(function (match, index) {
+         var item = parseMatchHistoryItem(match, team.name);
 
-         // Parsing date strings
-         team.matchHistory.forEach(function (match, index) {
-            // TODO check if parsing is done correctly
-            match.date = new Date(match.date);
-         });
+         if (shouldBeAddedToHistory(matchHistory, item, numberMatchesPerTeam)) {
+            matchHistory.push(item);
+         }
 
-         // Sorting by date descending
-         team.matchHistory.sort(function (a, b) {
-            if (a.date.getTime() > b.date.getTime()) {
-               return 1;
-            } else {
-               return -1;
-            }
-         });
+         if (item.inHome && shouldBeAddedToHistory(matchHistoryHome, item, numberMatchesPerTeam)) {
+            matchHistoryHome.push(item);
+         }
 
-         team.matchHistory.forEach(function (match, index) {
-            var item = parseMatchHistoryItem(match, team.name);
-
-            if (shouldBeAddedToHistory(matchHistory, item, numberMatchesPerTeam)) {
-               matchHistory.push(item);
-            }
-
-            if (item.inHome && shouldBeAddedToHistory(matchHistoryHome, item, numberMatchesPerTeam)) {
-               matchHistoryHome.push(item);
-            }
-
-            if (!item.inHome && shouldBeAddedToHistory(matchHistoryAway, item, numberMatchesPerTeam)) {
-               matchHistoryAway.push(item);
-            }
-         });
-
-         teamsInfo.push({
-            name: team.name,
-            performance: calculatePerformance(matchHistory),
-            detailed: false, // Controls if detailed information is visible
-            matchHistory: matchHistory,
-            matchHistoryHome: matchHistoryHome,
-            matchHistoryAway: matchHistoryAway
-         });
+         if (!item.inHome && shouldBeAddedToHistory(matchHistoryAway, item, numberMatchesPerTeam)) {
+            matchHistoryAway.push(item);
+         }
       });
-      return teamsInfo;
-   };
 
-   var $app = angular.module('teamPerformanceWidget');
+      teamsInfo.push({
+         name: team.name,
+         performance: calculatePerformance(matchHistory),
+         detailed: false, // Controls if detailed information is visible
+         matchHistory: matchHistory,
+         matchHistoryHome: matchHistoryHome,
+         matchHistoryAway: matchHistoryAway
+      });
+   });
+   return teamsInfo;
+};
 
-   $app.controller('appController',
-      ['$scope', '$controller', '$http', '$sce', 'kambiAPIService', 'kambiWidgetService',
-      function ($scope, $controller, $http, $sce, kambiAPIService, kambiWidgetService) {
+var TeamPerformance = Stapes.subclass({
+   constructor: function (name) {
+      this.scope = {};
 
-         angular.extend(this, $controller('widgetCoreController', {
-            '$scope': $scope
-         }));
+      CoreLibrary.init().on('ready', function (apiData) {
+         this.model = CoreLibrary.getData('someType', apiData.filter);
+         this.model.on('change', function (data) {
+            this.scope.args = Object.assign({
+               title: 'Football - Team Performance Indicator',
+               numberMatchesPerTeam: 6, // Maximum number of matches to show per team
+               listLimit: 3 // Set the list limit value to be used for pagination)
+            }, data.args);
+            this.scope.teams = parseTeamsInfo(data.tournaments[0].teams, this.scope.numberMatchesPerTeam);
+         }.bind(this));
+      }.bind(this));
 
-         // Default arguments, these will be overridden by the arguments from the widget api
-         $scope.defaultArgs = {
-            title: 'Football - Team Performance Indicator',
-            numberMatchesPerTeam: 6, // Maximum number of matches to show per team
-            listLimit: 3 // Set the list limit value to be used for pagination
-         };
+      this.view = rivets.bind(document.getElementById('main'), this.scope);
+      this.view.binders['box-css-class'] = this.boxCssClass;
+   },
 
-         $scope.Math = window.Math; // To be able to use Math functions in the template
+   boxCssClass: function (el, value) {
+      el.classList.add('kw-match-' + value);
+   }
+});
 
-         $scope.startFrom = 0;
-         $scope.teams = [];
+var teamPerformance = new TeamPerformance();
 
-         $scope.adjustHeight = function () {
-            // TODO maybe try to dinamically get these values?
-            // might not be possible to get detailedViewHeight due to the accordion animation
-            var headerHeight = 40;
-            var compactViewTeamInfoHeight = 85;
-            var tableLineHeight = 45;
-
-            var contentHeight = headerHeight;
-
-            var teams = $scope.teams;
-            var i = $scope.startFrom;
-            while (i < $scope.startFrom + $scope.args.listLimit && i < teams.length) {
-               if (teams[i].detailed) {
-                  contentHeight += compactViewTeamInfoHeight + teams[i].matchHistory.length * tableLineHeight;
-               } else {
-                  contentHeight += compactViewTeamInfoHeight;
-               }
-               i++;
-            }
-
-            $scope.setWidgetHeight(contentHeight + 1);
-         };
-
-         $scope.init().then(function () {
-            $http({
-               method: 'GET',
-               url: './mockdata.json'
-            }).then(function (response) {
-               var teams = response.data.tournaments[0].teams;
-               var teamsInfo = parseTeamsInfo(teams, $scope.args.numberMatchesPerTeam);
-               $scope.teams = teamsInfo;
-               $scope.setPages($scope.teams, $scope.args.listLimit);
-               $scope.adjustHeight();
-            });
-         });
-      }]);
-})();
+}).call(this);
