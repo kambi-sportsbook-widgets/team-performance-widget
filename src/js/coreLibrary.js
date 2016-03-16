21e52da65157b504cdff80e60a7e010e61566090
Michael Blom <michael@globalmouth.com>
2016-03-16 15:58:33 +0100

    Initialization of widget api

--- src/js/coreLibrary.js
+++ src/js/coreLibrary.js
@@ -2,20 +2,73 @@ var CoreLibrary = (function () {
 
    'use strict';
 
-   /**
-    * CoreLibrary code
-    */
-   return {
+   function checkStatus ( response ) {
+      if ( response.status >= 200 && response.status < 300 ) {
+         return response;
+      } else {
+         var error = new Error(response.statusText);
+         error.response = response;
+         throw error;
+      }
+   }
+
+   function parseJSON ( response ) {
+      return response.json();
+   }
+
+   var CoreLibrary = {
+      api: null,
+      config: {},
       init: function () {
          return new Promise(function ( resolve, reject ) {
-            setTimeout(function () { // "ajax" call
-               var apiData = {
-                  filter: 'football/england/premier_league/',
-                  args: {}
-               };
-               resolve(apiData);
-            }, 100);
-         });
+            if ( window.KambiWidget ) {
+               // For development purposes we might want to load a widget on it's own so we check if we are in an iframe, if not then load some fake data
+               if ( window.self === window.top ) {
+                  console.warn(window.location.host + window.location.pathname + ' is being loaded as stand-alone');
+                  fetch('mockSetupData.json')
+                     .then(checkStatus)
+                     .then(parseJSON)
+                     .then(function ( mockSetupData ) {
+                        console.debug('Loaded mock setup data');
+                        console.debug(mockSetupData);
+                        this.applySetupData(mockSetupData);
+                        resolve(this.config);
+                     }.bind(this))
+                     .catch(function ( error ) {
+                        console.debug('Request failed:', error);
+                        reject();
+                     });
+               } else {
+                  window.KambiWidget.apiReady = function ( api ) {
+                     this.api = api;
+                     console.debug('API Ready');
+                     this.requestSetup(function ( setupData ) {
+                        this.applySetupData(setupData);
+                        resolve(this.config.arguments);
+                     }.bind(this));
+                  }.bind(this);
+                  window.KambiWidget.receiveResponse = function ( dataObject ) {
+                     this.receiveRespone(dataObject);
+                  };
+               }
+            } else {
+               console.warn('Kambi widget API not loaded');
+               reject();
+            }
+         }.bind(this));
+      },
+
+      applySetupData: function ( setupData ) {
+         console.debug(setupData);
+         this.config = setupData;
+      },
+
+      requestSetup: function ( callback ) {
+         this.api.requestSetup(callback);
+      },
+
+      receiveRespone: function ( response ) {
+         console.debug(response);
       },
 
       getData: function ( type, filter ) {
@@ -24,5 +77,7 @@ var CoreLibrary = (function () {
          });
       }
    };
+
+   return CoreLibrary;
 
 })();
