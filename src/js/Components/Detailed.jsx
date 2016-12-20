f790b5992f7e0e3eef7abe96f7fa956dc1807591
Owain Stratton <owain@globalmouth.com>
2016-12-20 12:49:26 +0100

    KSBWI-867 adding the EventResultIndicator component to the Detailed view.

--- src/js/Components/Detailed.jsx
+++ src/js/Components/Detailed.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ParticipantResults from './ParticipantResults';
 import EventResult from './EventResult';
+import EventResultIndicator from './EventResultIndicator';
 
 const Detailed = ({ participants }) => {
    return (
@@ -9,14 +10,17 @@ const Detailed = ({ participants }) => {
             participants.map(participant =>
                <ParticipantResults key={participant.id} name={participant.name}>
                   {
-                     participant.lastEvents.map(event =>
-                        <EventResult
-                           key={event.start}
-                           homeName={event.homeName}
-                           homeScore={event.homeScore}
-                           awayName={event.awayName}
-                           awayScore={event.awayScore}
-                        />
+                     participant.lastEvents.map((event, idx) =>
+                        <div className='kw-eventresult-container'>
+                           <EventResultIndicator key={idx} result={event.result} />
+                           <EventResult
+                              key={event.start}
+                              homeName={event.homeName}
+                              homeScore={event.homeScore}
+                              awayName={event.awayName}
+                              awayScore={event.awayScore}
+                           />
+                        </div>
                      )
                   }
                </ParticipantResults>
